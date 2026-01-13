b156c0f32c67ad7b6c82df4eca13b21e9984e5e0
Oscar Serrano Yanez <2691820@tcs.com>
2026-01-13 16:58:49 -0600

    Se crea un useReducer con la instalacion de Shadcn ui

--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,16 @@ import { TrafficLight } from './01-useState/TrafficLight.tsx'
 import { TrafficLightWithEffect } from './02-useEffect/TrafficLightWithEffect.tsx'
 import { TrafficLightWithHook } from './02-useEffect/TrafficLightWithHook.tsx'
 import { PokemonPage } from './03-ejemplos/PokemonPage.tsx'
+import FocusScreen from './04-useRef/FocusScreen.tsx'
+import { TasksApp } from './05-useReducer/TaskApp.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     {/* <TrafficLight /> */}
     {/* <TrafficLightWithEffect /> */}
     {/* <TrafficLightWithHook /> */}
-    <PokemonPage />
+    {/* <PokemonPage /> */}
+    {/* <FocusScreen /> */}
+    <TasksApp />
   </StrictMode>,
 )
